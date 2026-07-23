2baf859391775aadc7f5fcccb2f59015749f07ca
Sonacatherine06 <sonacatherine06@gmail.com>
2026-07-23 12:21:21 +0530

    Add files via upload

--- 03-Hobby-motor/hobby_motor.ino
+++ 03-Hobby-motor/hobby_motor.ino
@@ -1,1 +1,13 @@
+int motor = 8;
 
+void setup() {
+  pinMode(motor, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(motor, HIGH); // Motor ON
+  delay(3000);
+
+  digitalWrite(motor, LOW);  // Motor OFF
+  delay(3000);
+}
